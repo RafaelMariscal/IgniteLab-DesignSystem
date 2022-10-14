f205de1a4f8fc50d7ac1f37898658e9484690017
Rafael Mariscal <rafael_mariscal_@outlook.com>
2022-10-13 21:34:57 -0300

    Add/Text Component Props and Variants

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,6 @@ import './styles/global.css'
 export function App() {
 
   return (
-    <h1 className='font-bold text-5xl'>Teste</h1>
+    <h1 className='font-bold text-5xl'>Hello World</h1>
   )
 }
