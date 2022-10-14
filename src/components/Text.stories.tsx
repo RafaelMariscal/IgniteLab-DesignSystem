accc13bab9e8d9cdfb7f51991c273e3bd188655b
Rafael Mariscal <rafael_mariscal_@outlook.com>
2022-10-14 15:42:09 -0300

    Setup/Hiding storybook Text Custom varaint's table

--- src/components/Text.stories.tsx
+++ src/components/Text.stories.tsx
@@ -6,6 +6,15 @@ export default {
   component: Text,
   args: {
     children: 'Random Text',
+    size: 'md'
+  },
+  argTypes: {
+    size: {
+      options: ['sm', 'md', 'lg',],
+      control: {
+        type: 'inline-radio'
+      }
+    }
   }
 } as Meta<TextProps>
 
@@ -26,5 +35,12 @@ export const CustomComponent: StoryObj<TextProps> = {
     children: (
       <p>Testing</p>
     )
+  },
+  argTypes: {
+    children: {
+      table: {
+        disable: true
+      }
+    }
   }
 }
