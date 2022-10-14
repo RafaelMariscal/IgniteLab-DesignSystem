4f3c27033c7ebfaa943db25aaead83597f5b68fb
Rafael Mariscal <rafael_mariscal_@outlook.com>
2022-10-14 16:53:30 -0300

    Start/TextInput componnet Composition Pattern

---
+++ src/components/TextInput.stories.tsx
@@ -0,0 +1,23 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { TextInput, TextInputRootProps } from './TextInput'
+
+export default {
+  title: 'Components/TextInput',
+  component: TextInput.Root,
+  args: {
+    children: (
+      <TextInput.Input placeholder='Random Placeholder' />
+    )
+  },
+  argTypes: {
+    argTypes: {
+      children: {
+        table: {
+          disable: true
+        }
+      },
+    }
+  }
+} as Meta<TextInputRootProps>
+
+export const Default: StoryObj<TextInputRootProps> = {}
