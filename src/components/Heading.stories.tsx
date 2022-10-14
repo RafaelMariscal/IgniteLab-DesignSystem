3125e5316c86c38eb609fab285f9f7123c1b1de6
Rafael Mariscal <rafael_mariscal_@outlook.com>
2022-10-14 15:56:39 -0300

    Add/Heading Component Props and Variants

--- src/components/Heading.stories.tsx
+++ src/components/Heading.stories.tsx
@@ -1,11 +1,11 @@
 import { Meta, StoryObj } from '@storybook/react'
-import { Text, TextProps } from './Text'
+import { Heading, HeadingProps } from './Heading'
 
 export default {
-  title: 'Components/Text',
-  component: Text,
+  title: 'Components/Heading',
+  component: Heading,
   args: {
-    children: 'Random Text',
+    children: 'Random h2',
     size: 'md'
   },
   argTypes: {
@@ -16,31 +16,31 @@ export default {
       }
     }
   }
-} as Meta<TextProps>
+} as Meta<HeadingProps>
 
-export const Default: StoryObj<TextProps> = {}
-export const Small: StoryObj<TextProps> = {
+export const Default: StoryObj<HeadingProps> = {}
+export const Small: StoryObj<HeadingProps> = {
   args: {
     size: 'sm'
   }
 }
-export const Large: StoryObj<TextProps> = {
+export const Large: StoryObj<HeadingProps> = {
   args: {
     size: 'lg'
   }
 }
-export const CustomComponent: StoryObj<TextProps> = {
+export const CustomComponent: StoryObj<HeadingProps> = {
   args: {
     asChild: true,
     children: (
-      <p>Text with P tag</p>
+      <h1>Heading with h1</h1>
     )
   },
   argTypes: {
     children: {
       table: {
         disable: true
-      }
+      },
     },
     asChild: {
       table: {
